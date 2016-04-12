46cc3cd88be2e4aa217d93e8ad7730ac39927057
Jonathan Lake <jwtlake@gmail.com>
2016-04-12 12:16:52 -0700

    fixed folder naming

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var mybot = new Discord.Client();
 mybot.on("message", function(message){
     if(message.content === "!test") {
        mybot.joinVoiceChannel(message.author.voiceChannel, function(error, voice) {
-           voice.playFile("./res/AndHisNameIsJohnCena.mp3", {"volume":1.0}, function(error, intent) {
+           voice.playFile("./audio/AndHisNameIsJohnCena.mp3", {"volume":1.0}, function(error, intent) {
              if (error) console.log(error.stack);
   	             intent.on("end", function() {
                      mybot.leaveVoiceChannel();
