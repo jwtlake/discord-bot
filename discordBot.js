f9fbae075cb3644c531f134311ae8d7f5ec511e0
Jonathan Lake <jwtlake@gmail.com>
2016-04-24 14:54:29 -0700

    added check to verify that the file exists before trying to play

--- discordBot.js
+++ discordBot.js
@@ -6,23 +6,23 @@ var isConnected = false;
 mybot.autoReconnect = true;
 
 //watch for !commands
-mybot.on("message", function(message){
-    //check if message for bot
-    if(message.content.charAt(0) === "!" && message.content.length > 1) {
-       mybot.joinVoiceChannel(message.author.voiceChannel, function(error, voice) {
-           //AndHisNameIsJohnCena
-           voice.playFile("./audio/" + message.content.slice(1) + ".mp3", {"volume":1.0}, function(error, intent) {
-             if (error) console.log(error.stack);
-  	             intent.on("end", function() {
-                     mybot.leaveVoiceChannel();
-                 });
-           });
-      });
-    }
+mybot.on("message", function(message) {
+	//check if message for bot
+	if(message.content.charAt(0) === "!" && message.content.length > 1) {
+		//get requested file from command
+		var file = "./audio/" + message.content.slice(1) + ".mp3";
+		//check if it exists
+		path.exists(file, function(exists) { 
+			if (exists) { 
+				//play audio file
+				playAudioFile(file);
+			}
+		});
+	}
 });
 
 //public functions
-function login(email, password){
+function login(email, password) {
   mybot.login(email, password)
   .then(function(success){
     console.log(success);
@@ -33,13 +33,27 @@ function login(email, password){
   });
 }
 
-function logout(){
+function logout() {
   mybot.logout();
   isConnected = false;
 }
 
-function getStatus(){
+function getStatus() {
   return isConnected;
+}
+
+function playAudioFile(file) {
+	//connect to the current voice channel of commanding user
+	mybot.joinVoiceChannel(message.author.voiceChannel, function(error, voice) {
+		//play audio file
+		voice.playFile(file, {"volume":1.0}, function(error, intent) {
+			if (error) console.log(error.stack);
+				//diconnect after playing
+				intent.on("end", function() {
+					mybot.leaveVoiceChannel();
+				});
+		});
+	});
 }
 
 
